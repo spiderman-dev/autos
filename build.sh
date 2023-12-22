677a9d041df9171c4f9f166b7bd872e5c962a473
spiderman-dev <152533850+spiderman-dev@users.noreply.github.com>
2023-12-22 18:26:36 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-manage.py createsuperuser --username=uno --email=jheralf09@gmail.com --password=JAHA0109hr --noinput
+manage.py createsuperuser 
 
 
 python manage.py migrate
