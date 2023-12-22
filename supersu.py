ca17d109826b878efe1070c4b49d4e97af27539b
spiderman-dev <152533850+spiderman-dev@users.noreply.github.com>
2023-12-22 17:59:55 -0400

    Update supersu.py

--- supersu.py
+++ supersu.py
@@ -1,15 +1,1 @@
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-username = 'admin'
-email = 'spiderman.develop@gmail.com'
-password = 'JAHA0109'
-
-try:
-    # Verificar si ya existe un superusuario con ese nombre de usuario
-    existing_user = User.objects.get(username=username)
-    print("Ya existe un superusuario")
-except User.DoesNotExist:
-    # Crear un nuevo superusuario si no existe
-    User.objects.create_superuser(username=username, email=email, password=password)
-
+print("Holau")
