14f85831284272ccb8b67dcc280b104e0fb62848
spiderman-dev <152533850+spiderman-dev@users.noreply.github.com>
2023-12-22 16:47:04 -0400

    Update settings.py

--- sistema/settings.py
+++ sistema/settings.py
@@ -110,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'es-es'
+LANGUAGE_CODE = 'es-ve'
 
-TIME_ZONE = 'UTC-4'
+TIME_ZONE = 'America/Caracas'
 
 USE_I18N = True
 
