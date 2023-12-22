fda80c9e9e4813fce67912fd97648726891d604a
spiderman-dev <152533850+spiderman-dev@users.noreply.github.com>
2023-12-22 18:10:36 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py createsuperuser --no-input --username=admin
+python manage.py createsuperuser --no-input --username=admin --email=spiderman.develop@gmail.com
 python manage.py migrate
